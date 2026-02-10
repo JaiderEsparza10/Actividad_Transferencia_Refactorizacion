cdde9dfb1cbd0d8bb2110e8c70957f44cdf690ab
Jaider Esparza <xenno2003@gmail.com>
2026-02-10 15:13:03 -0500

    Se creó el archivo para la solución del punto 1 y el modulo del mismo. Se intentó instalar el nmp pero hubo un error, queda pendiente corregirlo+

---
+++ Ejercicio1.js
@@ -0,0 +1,24 @@
+import { usuarios, postUsuarios } from './Modules/Modulo1.js';
+
+usuarios()
+    .then((usuario) => {
+        // Se inicializa una constante que se le asigna con un arreglo vacío para guardar el resultado final
+        const resultados = [];
+        // recorre el arreglo de los usuarios
+        usuario.map(({ id, name }) => {
+            // Llama a la función postUsuarios la cual obtiene los post de cada usuario por medio del id
+            postUsuarios(id)
+                .then((post) => {
+                    // En el arreglo resultados se guarda el nombre del usuario y la cantidad de posts que tiene
+                    resultados.push({
+                        nombre: name,
+                        CantidadPosts: post.length
+                    });
+                    // Condición que compara si la cantidad de resultados es igual a la cantidad total de usuarios
+                    if (resultados.length === usuario.length) {
+                        // Imprime el nombre del usuario y la cantidad de posts
+                        console.log(resultados);
+                    }
+                })
+        })
+    })
